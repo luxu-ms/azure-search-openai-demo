5e8b7574d68c309a4d9881967623fb17c7d68859
Lyle Xu <luxu@microsoft.com>
2023-06-19 10:48:40 +0800

    refine deploy script

--- deploy.ps1
+++ deploy.ps1
@@ -1,48 +1,17 @@
-if($args.Length -lt 7){
-    Write-Host "The required parameters should be provided."
-    Write-Host ".\deploy.ps1 <environment name> <project name> <dev center name> <environment type> <catalog name> <catalog item name> <principal id>"
-    exit
-}
-
-$environmentName = $args[0]
-$projectName = $args[1]
-
-function Get-UniqueString ([string]$id, $length=13)
-{
-    $hashArray = (new-object System.Security.Cryptography.SHA512Managed).ComputeHash($id.ToCharArray())
-    -join ($hashArray[1..$length] | ForEach-Object { [char]($_ % 26 + [byte][char]'a') })
-}
-
-$resourceGroup = "$projectName-$environmentName"
-$resourceToken = Get-UniqueString($resourceGroup)
-
-$backendServiceName = "app-backend-$resourceToken"
-$storageAccountName = "st$resourceToken"
-$searchServiceName = "gptkb-$resourceToken"
-$formRecognizerServiceName = "cog-fr-$resourceToken"
-$appServicePlanName = "plan-$resourceToken"
-$openAiServiceName = "cog-$resourceToken"
-$devcenterName = $args[2]
-$environmentType = $args[3]
-$catalogName = $args[4]
-$catalogItemName = $args[5]
-$principalId = $args[6]
-
-Write-Host "Provisioning Azure resources..."
-$result = az devcenter dev environment create --dev-center-name $devcenterName `
-                                    --project-name $projectName `
-                                    --environment-name $environmentName `
-                                    --environment-type $environmentType `
-                                    --catalog-name $catalogName `
-                                    -e $catalogItemName `
-                                    --parameters "{'environmentName':'$environmentName','principalId':'$principalId','backendServiceName':'$backendServiceName','storageAccountName':'$storageAccountName', 'searchServiceName':'$searchServiceName', 'formRecognizerServiceName':'$formRecognizerServiceName', 'appServicePlanName':'$appServicePlanName', 'openAiServiceName':'$openAiServiceName'}" 
+$resourceGroup = Read-Host "Please enter name of resource group"
+$appServicePlanName = Read-Host "Please enter name of app service plan e.g. plan-xxx"
+$backendServiceName = Read-Host "Please enter name of app service e.g. app-backend-xxx"
+$storageAccountName = Read-Host "Please enter name of storge account e.g. stxxx"
+$searchServiceName = Read-Host "Please enter name of search service e.g. gptkb-xxx"
+$formRecognizerServiceName = Read-Host "Please enter name of form recognizer .e.g. cog-fr-xxx"
+$location=Read-Host "Please enter location e.g. eastus"
 
 Write-host "Deploying App service..."
 Set-Location 'app'
 .\start.ps1
 
 Set-Location '../backend' 
-az webapp up -n $backendServiceName -g $resourceGroup -p $appServicePlanName
+az webapp up -n $backendServiceName -g $resourceGroup -p $appServicePlanName -l $location
 
 Write-Host "Uploading gpt docs..."
 Set-Location "../../"
